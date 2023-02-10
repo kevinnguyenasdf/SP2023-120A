580e2ce7758f3f25fefb56e29690df8a528a1996
Kevin Nguyen <kevinnguyenasdf@yahoo.com>
2023-02-10 08:57:03 -0800

    Week 3 SI

---
+++ isEven.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int num = 5;
+	std::cout << isEven(num) << std::endl;
+	
+	num = 3;
+	std::cout << isEven(num) << std::endl;
+
+	num = 7;
+	std::cout<< isEven(num) << std::endl;
+}
---
+++ isEven_nofunction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main(){
+	int num = 5;
+	if(num % 2 == 0){
+		std::cout<<true<<std::endl;
+    }
+    else{
+        std::cout<<false<<std::endl;
+    }
+        num = 3;
+    if(num % 2 == 0){
+            std::cout<<true<<std::endl;
+    }
+    else{
+        std::cout<<false<<std::endl;
+    }
+    num = 7;
+    if(num % 2 == 0){
+            std::cout<<true<<std::endl;
+    }
+    else{
+        std::cout<<false<<std::endl;
+    }
+
+}
